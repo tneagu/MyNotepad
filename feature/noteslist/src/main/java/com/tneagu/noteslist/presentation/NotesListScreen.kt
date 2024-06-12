f216a3bba286fafd4bb5789fa6bda2a19131edd3
Tiberiu Neagu <neagutibi@gmail.com>
2024-06-12 15:00:42 +0300

    ui updates

--- feature/noteslist/src/main/java/com/tneagu/noteslist/presentation/NotesListScreen.kt
+++ feature/noteslist/src/main/java/com/tneagu/noteslist/presentation/NotesListScreen.kt
@@ -1,33 +1,81 @@
 package com.tneagu.noteslist.presentation
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallFloatingActionButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.tneagu.domain.entities.Note
+import com.tneagu.noteslist.R
 import com.tneagu.noteslist.presentation.model.NotesState
 import com.tneagu.noteslist.presentation.ui.NotesList
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NotesListScreen(
     state: NotesState,
     onNoteClick: (Note) -> Unit
 ) {
 
-    Column(
-        modifier = Modifier.padding(vertical = 16.dp)
-    ) {
-        when (state) {
-            is NotesState.Loaded -> {
-                NotesList(
-                    notes = state.notes,
-                    onNoteClick = onNoteClick,
-                )
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .navigationBarsPadding(),
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(stringResource(R.string.notes_list_title))
+                }
+            )
+        },
+        floatingActionButton = {
+            SmallFloatingActionButton(
+                onClick = { },
+                containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                contentColor = MaterialTheme.colorScheme.secondary
+            ) {
+                Icon(Icons.Filled.Add, "Small floating action button.")
             }
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            when (state) {
+                is NotesState.Loaded -> {
+                    Box(modifier = Modifier.padding(vertical = 16.dp)) {
+                        NotesList(
+                            notes = state.notes,
+                            onNoteClick = onNoteClick,
+                        )
+                    }
 
-            NotesState.NotInitialized -> {}
-            NotesState.Loading -> {}
+                }
+
+                NotesState.NotInitialized -> {}
+                NotesState.Loading -> {}
+            }
         }
     }
+
 }
